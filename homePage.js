d8dea4b8bd7150195d9447145b449a36b1f67563
Ken O'Hanlon <fj7615@wayne.edu>
2021-06-08 18:12:19 -0400

    Refactor for separate JS file. Addition of company logo to homepage -KO

---
+++ homePage.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navigationMenu").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("navigationMenu").style.width = "0%";
+}
