ac75e495196779d621451e75104989b6ea80b498
Ken O'Hanlon <fj7615@wayne.edu>
2021-06-22 22:08:16 -0400

    Fixed nav menu logo sizing -KO

--- homePage.js
+++ homePage.js
@@ -2,7 +2,7 @@ function openNav() {
     let navBarOpen = true;
     let x = window.innerWidth;
     console.log("Screen width: " + x);
-    if(window.innerWidth > "701"){
+    if(window.innerWidth > "829"){
         document.getElementById("navigationMenu").style.width = "300px";
         console.log("Screen width more than 700px");
     } else{
